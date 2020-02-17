ef66dcd02ac1a6cd7fec92371dffb3ab68ecd408
thejohnwatt <j.watt10@gmail.com>
2020-02-16 20:22:49 -0800

    added image in between about and film that is in the background and will be fixed while divs scroll over. added videos to film

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import facebook from '../assests/facebook.png'
-import twitter from '../assests/twitter.png'
 import youtube from '../assests/youtube.png'
 import instagram from '../assests/instagram.png'
 
@@ -10,18 +9,18 @@ function About() {
 
     return (
         <div>
-            <div className='socials-contents'>
+            <div id='about' className='socials-contents'>
                 <div className='socials-header'>
                     <h1>SOCIAL MEDIA</h1>
                 </div>
-                    <div className='social-icons'>
+                    <div  className='social-icons'>
                         <a href='https://www.youtube.com/user/boomshakalaka33' target="_blank"><img src={youtube} alt='Youtube' /></a>
                         <a href='https://www.facebook.com/gavenayala/' target="_blank"><img src={facebook} alt='Facebook'/></a>
                         <a href='https://www.instagram.com/ayalamedia/' target="_blank"><img src={instagram} alt='Instagram'/></a>
                     </div>
                     <h2>Stay up to date on conntent by following us on social media!</h2>
                 </div>
-                <div id='about' className='about-container'>   
+                <div  className='about-container'>   
                 <div className='about-contents'>
                     <div className='heading'><h1>ABOUT</h1></div>
                     <p>Bro ipsum dolor sit amet reverse camber park rat air poaching skid lid, north shore schwag air free ride ski bum ollie frozen chicken heads. Taco DH wheelie drop smear greasy frozen chicken heads avie bomb hole gondy hurl carcass tele. Wheels bro grab back country couloir air. Shreddin ripper dope huckfest afterbang brain bucket fatty ski bum punter gaper hurl carcass flow face shots greasy cork. McTwist lid yard sale washboard shred. Free ride derailleur rail, road rash lid smear huck gorby. White room wheelie booter, air sharkbite schwag cornice first tracks air gapers freshies caballerial endo.</p>
--- src/components/film.js
+++ src/components/film.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
+import motoimage from '../assests/motoimage1.jpg'
+
 function Film() {
 
 
     return (
-        <div>
+        <div className='film-container'>
+            <div className='film-image'>
+                {/* <img src={motoimage} alt='film-header' /> */}
+            </div>
+            <div id='film' className='film-contents'>
+                <div className='film-header'>
+                    <h1>FILM</h1>
+                </div>
+                <p>
+                Bro ipsum dolor sit amet air granny gear first tracks gondy. Poaching death cookies sharkbite, chain ring gapers frozen chicken heads japan air glades spin backside carve 360 chain suck. Taco nose press wheels, face shots hot dogging sucker hole hardtail line poaching. Mute death cookies stoked bowl road rash bump brain bucket stunt punter epic scream greasy first tracks shuttle whip. 
+                </p>
+                <div className='videos-container'>
+                <iframe width="33%" height="300" src="https://www.youtube.com/embed/-ZrYWZ3cWMo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="33%" height="300" src="https://www.youtube.com/embed/Aqztfey9Plc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="33%" height="300" src="https://www.youtube.com/embed/-ZrYWZ3cWMo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      
+                </div>
+            </div>
 
         </div>
     )
