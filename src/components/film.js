832d195011d9cb8df0090c952cb9aeb74fed50bb
thejohnwatt <j.watt10@gmail.com>
2020-02-17 01:22:44 -0800

    added carousel for film

--- src/components/film.js
+++ src/components/film.js
@@ -1,34 +1,34 @@
 import React from 'react'
 
+import AliceCarousel from 'react-alice-carousel';
+
+import './alice-carousel/alice-carousel.scss'
 function Film() {
 
 
     return (
         <div className='film-container'>
             <div id='film' className='film-image'>
-                {/* <img src={motoimage} alt='film-header' /> */}
                 <div className='film-header'>
                 <h1>FILM</h1>
                 </div>
             </div>
             <div  className='film-contents'>
-                
-                    
-                
-                <p>
+                {/* <p>
                     Ayala Media sets the bar for the highest quality in video production and editing. Checkout our <a href='https://www.youtube.com/user/boomshakalaka33'>YouTube</a> page for more content.
-                </p>
+                </p> */}
                 <div className='videos-container'>
-                    <div className='row1'>
-                        <iframe title='film-1' width="30%" height="300" src="https://www.youtube.com/embed/-ZrYWZ3cWMo" frameborder="0" allow="accelerometer; autoplay;encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <iframe title='film-2'  width="30%" height="300" src="https://www.youtube.com/embed/Aqztfey9Plc" frameborder="0" allow="accelerometer; autoplay;encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <iframe title='film-3'  width="30%" height="300" src="https://www.youtube.com/embed/FDRNNt6cgqg" frameborder="0" allow="accelerometer; autoplay;encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                    <div className='row2'>
-                        <iframe title='film-1' width="30%" height="300" src="https://www.youtube.com/embed/-ZrYWZ3cWMo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <iframe title='film-2'  width="30%" height="300" src="https://www.youtube.com/embed/Aqztfey9Plc" frameborder="0" allow="accelerometer; autoplay;encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <iframe title='film-3'  width="30%" height="300" src="https://www.youtube.com/embed/FDRNNt6cgqg" frameborder="0" allow="accelerometer; autoplay;encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
+                    <AliceCarousel>
+                      
+                        <iframe className="vid" width="100%"  height="550" src="https://www.youtube.com/embed/-ZrYWZ3cWMo" />
+                       
+                        <iframe className="vid" width="100%" height="550" src="https://www.youtube.com/embed/Aqztfey9Plc" />
+                       
+                        <iframe className="vid" width="100%" height="550" src="https://www.youtube.com/embed/FDRNNt6cgqg" />
+                       
+                        <iframe className="vid" width="100%" height="550" src="https://www.youtube.com/embed/qbFIrnYy3P0" />
+                       
+                    </AliceCarousel>
                 </div>
             </div>
 
