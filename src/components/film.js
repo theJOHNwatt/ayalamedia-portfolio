1d0967e66503d9fb06ac90801602a5121a8f5a74
thejohnwatt <j.watt10@gmail.com>
2020-02-16 20:31:28 -0800

    styles added to film.

--- src/components/film.js
+++ src/components/film.js
@@ -15,13 +15,12 @@ function Film() {
                     <h1>FILM</h1>
                 </div>
                 <p>
-                Bro ipsum dolor sit amet air granny gear first tracks gondy. Poaching death cookies sharkbite, chain ring gapers frozen chicken heads japan air glades spin backside carve 360 chain suck. Taco nose press wheels, face shots hot dogging sucker hole hardtail line poaching. Mute death cookies stoked bowl road rash bump brain bucket stunt punter epic scream greasy first tracks shuttle whip. 
+                    Ayala Media sets the bar for the highest quality in video production and editing. Checkout our youtube page for more content.
                 </p>
                 <div className='videos-container'>
-                <iframe width="33%" height="300" src="https://www.youtube.com/embed/-ZrYWZ3cWMo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe width="33%" height="300" src="https://www.youtube.com/embed/Aqztfey9Plc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <iframe width="33%" height="300" src="https://www.youtube.com/embed/-ZrYWZ3cWMo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      
+                <iframe width="30%" height="300" src="https://www.youtube.com/embed/-ZrYWZ3cWMo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="30%" height="300" src="https://www.youtube.com/embed/Aqztfey9Plc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="30%" height="300" src="https://www.youtube.com/embed/FDRNNt6cgqg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
 
